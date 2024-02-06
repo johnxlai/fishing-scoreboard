b98b35b2cde79615eb267187496b1ac70a3d4119
John Lai <lai.john.py@gmail.com>
2024-02-06 09:48:48 -0500

    Add basic style and pseudocode

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useState } from 'react';
 
-const Form = () => {
-  //create one user
-  // create a form with dropdown for fish type
-  //display user with points
+//fishing scoreboard will contain a form to add points to a player
+//form will contain a select dropdown to select a player and a fish
+//form will contain a button to submit the form
+//Scoreboard will contain a list of players and their points
+//Scoreboard will contain a total points for all players
+//Add local storage to store the players and their points
+//Add a reset button to reset the players and their points
 
+const Form = () => {
   const [players, setPlayers] = useState([
     {
       id: 1,
@@ -20,6 +24,21 @@ const Form = () => {
     {
       id: 3,
       name: 'John',
+      points: 0,
+    },
+    {
+      id: 4,
+      name: 'Dre',
+      points: 0,
+    },
+    {
+      id: 5,
+      name: 'Dennis',
+      points: 0,
+    },
+    {
+      id: 6,
+      name: 'Jackal',
       points: 0,
     },
   ]);
@@ -41,7 +60,6 @@ const Form = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     // Add your form submission logic here
-
     console.log('Form data submitted:', formData);
     updatePoints(formData.selectedOption);
   };
@@ -79,7 +97,6 @@ const Form = () => {
         ))}
       </ul>
 
-      {/* <h2>{player}</h2> */}
       <h4>Player points: {points}</h4>
 
       {formData.selectedName && (
@@ -88,10 +105,13 @@ const Form = () => {
         </h5>
       )}
 
-      <form onSubmit={handleSubmit} className='max-w-sm mx-auto'>
+      <form
+        onSubmit={handleSubmit}
+        className='max-w-sm mx-auto bg-green-600 p-5 mt-5 rounded-md'
+      >
         <label
           htmlFor='playerName'
-          className='block mb-2 text-sm font-medium text-white'
+          className='block my-2 text-sm font-medium text-white'
         >
           Player Name
         </label>
@@ -100,7 +120,7 @@ const Form = () => {
           value={formData.selectedName}
           onChange={handleChange}
           id='playerName'
-          className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-3'
         >
           <option value='john'>John</option>
           <option value='che'>Che</option>
@@ -109,16 +129,16 @@ const Form = () => {
 
         <label
           htmlFor='species'
-          className='block mb-2 text-sm font-medium text-white'
+          className='block my-2 text-sm font-medium text-white'
         >
-          Select your fish
+          Fish Caught
         </label>
         <select
           name='selectedOption'
           value={formData.selectedOption}
           onChange={handleChange}
           id='species'
-          className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4'
         >
           <option value='walleye'>Walleye</option>
           <option value='perch'>Perch</option>
@@ -126,9 +146,9 @@ const Form = () => {
           <option value='whiteFish'>White Fish</option>
         </select>
         <input
-          className='text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900'
+          className='text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 w-full uppercase tracking-wider'
           type='submit'
-          value='add'
+          value='Add'
         />
       </form>
     </div>
