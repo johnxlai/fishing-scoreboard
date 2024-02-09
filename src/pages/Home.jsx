f79678897dcab8e645b5ca548b6f25cd0c4acdb5
John Lai <lai.john.py@gmail.com>
2024-02-09 15:28:10 -0500

    Fix class to classname

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
       <main>
         <section className='bg-dark'>
           <div className='flex flex-col items-center justify-center min-h-screen text-white layout'>
-            <h1 class='mt-5'>Fishing Scoreboard</h1>
+            <h1 className='mt-5'>Fishing Scoreboard</h1>
             <Form />
             <footer className='my-3'>🐟🐟🐟</footer>
           </div>
