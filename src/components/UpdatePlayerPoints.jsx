ae0072160fe9f81192ac04a722192614677b2562
John Lai <lai.john.py@gmail.com>
2024-05-13 15:12:06 -0400

    Update point system

--- src/components/UpdatePlayerPoints.jsx
+++ src/components/UpdatePlayerPoints.jsx
@@ -71,17 +71,14 @@ const EditPlayer = ({ players }) => {
           className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4'
         >
           <option value=''>Select a fish</option>
-          <option value={1}>1 Perch/Panfish</option>
-          <option value={2}>2 Whitefish/Cisco</option>
-          <option value={3}>3 Walleye 43 cm</option>
-          <option value={4}>4 Walleye 43-60 cm</option>
-          <option value={5}>5 Walleye 60+ cm</option>
-          <option value={6}>6 Pike/Bass</option>
-          <option value={7}>7 Ling/Drum/Burbot</option>
-          <option value={8}>8 Trout/Muskie</option>
-          <option value={-5}>-5 Mudpuppy</option>
+          <option value={5}>Walleye Keeper 5</option>
+          <option value={4}>Muskie 4</option>
+          <option value={3}>Walleye 3</option>
+          <option value={2}>Pike 2</option>
+          <option value={1}>Bass 1</option>
+          <option value={-2}>Rock Bass -2</option>
+          <option value={3}>Longest Fish 3</option>
         </select>
-
         <button
           className='disabled:cursor-not-allowed disabled:opacity-80 disabled:bg-gray-500 disabled:border-gray-600 text-white hover:text-white border border-green-500 bg-green-500 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 w-full uppercase tracking-wider '
           type='submit'
