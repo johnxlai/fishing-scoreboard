4a34770c14102076910338634771ab32ac1bac97
John Lai <lai.john.py@gmail.com>
2024-05-13 13:39:34 -0400

    Update total points components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,7 @@ import { playersCollectionRef } from '../lib/firestore.collections';
 // import RealTimePlayers from '@/components/RealTimePlayers';
 // import AddPlayer from '@/components/AddPlayer';
 import UpdatePlayerPoints from '@/components/UpdatePlayerPoints';
+import TotalPoints from '@/components/TotalPoints';
 
 export default function Home() {
   const [players, setPlayers] = useState([]);
@@ -55,15 +56,49 @@ export default function Home() {
                       key={player.id}
                       className='bg-gray-700 flex gap-3 justify-between items-center p-3 rounded-lg text-white text-sm '
                     >
-                      <span className=''>{i + 1}</span>
-                      <span className='font-bold uppercase'>
-                        {player.data.name}
-                      </span>
-                      <div className='flex flex-col justify-center items-center bg-purple-800 p-2 rounded-sm'>
-                        <span className='text-purple-100 font-bold text-xs'>
-                          Pts:
+                      <div className='w-full'>
+                        <span className=''>{i + 1}</span>
+                        <span className='font-bold uppercase'>
+                          {player.data.name}
                         </span>
-                        <span className='font-bold'>{player.data.points}</span>
+
+                        <div className='flex flex-col justify-center items-center bg-purple-800 p-2 rounded-sm'>
+                          <span className='text-purple-100 font-bold text-xs'>
+                            Pts:
+                          </span>
+                          <span className='font-bold'>
+                            {player.data.points}
+                          </span>
+                        </div>
+                      </div>
+                      <div className='fishes w-full flex flex-col'>
+                        <span className='font-bold uppercase mr-2'>
+                          perch: {player.data.perch}
+                        </span>
+                        <span className='font-bold uppercase mr-2'>
+                          whitefish: {player.data.whitefish}
+                        </span>{' '}
+                        <span className='font-bold uppercase mr-2'>
+                          walleyeSm: {player.data.walleyeSm}
+                        </span>{' '}
+                        <span className='font-bold uppercase mr-2'>
+                          walleyeMd: {player.data.walleyeMd}
+                        </span>{' '}
+                        <span className='font-bold uppercase mr-2'>
+                          walleyeLg: {player.data.walleyeLg}
+                        </span>{' '}
+                        <span className='font-bold uppercase mr-2'>
+                          pikeBass: {player.data.pikeBass}
+                        </span>{' '}
+                        <span className='font-bold uppercase mr-2'>
+                          ling: {player.data.ling}
+                        </span>{' '}
+                        <span className='font-bold uppercase mr-2'>
+                          trout: {player.data.trout}
+                        </span>{' '}
+                        <span className='font-bold uppercase mr-2'>
+                          mudpuppy: {player.data.mudpuppy}
+                        </span>{' '}
                       </div>
                       {/* <button onClick={() => deletePlayer(player.id)}>
                         delete
@@ -71,18 +106,7 @@ export default function Home() {
                     </li>
                   ))}
               </ul>
-
-              <div className='flex items-center justify-center m-3'>
-                <span className='bg-gray-600 text-purple-300 py-1 px-2 uppercase font-bold text-sm'>
-                  Total Points:
-                </span>
-                <span className='bg-purple-400 text-white py-1 px-2 uppercase font-bold text-sm'>
-                  {players.reduce(
-                    (acc, player) => acc + parseInt(player.data.points),
-                    0
-                  )}
-                </span>
-              </div>
+              <TotalPoints players={players} />
             </div>
             {/* <RealTimePlayers /> */}
             {/* <AddPlayer /> */}
